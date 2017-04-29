db89f9d65600ab7523e1dea1de0aae23e3fd162c
Faelivrin <jurasz.do@gmail.com>
2017-04-29 18:28:06 +0200

    Added login template

--- admin-portal/src/app/app.module.ts
+++ admin-portal/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { routing } from "./app.routing";
 import {MaterialModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import 'hammerjs';
@@ -9,6 +10,8 @@ import 'hammerjs';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { LoginComponent } from './components/login/login.component';
+
+import {LoginService} from './services/login.service';
 
 @NgModule({
   declarations: [
@@ -20,10 +23,13 @@ import { LoginComponent } from './components/login/login.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    routing,
     MaterialModule, 
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    LoginService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- admin-portal/src/app/components/login/login.component.ts
+++ admin-portal/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+	private credentials = {'username': '', 'password' : '' };
+	private loggedIn = false;
+
+
+  constructor(private loginService: LoginService) { }
+
+  onSubmit(){
+  	this.loginService.sendCredential(this.credentials.username, this.credentials.password).subscribe(
+  		res => {
+  			console.log(res);
+  			localStorage.setItem("xAuthToken", res.json().token);
+  			this.loggedIn = true;
+  			location.reload();
+  		}, error =>{
+  			console.log(error);
+  		}
+  	);
+  }
 
   ngOnInit() {
   }
