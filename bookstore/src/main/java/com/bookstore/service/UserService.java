1473577eea860edc3aafa437a96ed5dd5c0de2e1
Faelivrin <jurasz.do@gmail.com>
2017-04-29 14:11:47 +0200

    Added user data

---
+++ bookstore/src/main/java/com/bookstore/service/UserService.java
@@ -0,0 +1,14 @@
+package com.bookstore.service;
+
+import com.bookstore.domain.User;
+import com.bookstore.domain.security.UserRole;
+
+import java.util.Set;
+
+/**
+ * Created by Dominik on 24.04.2017.
+ */
+public interface UserService {
+
+    User createUser(User user, Set<UserRole> userRoles);
+}
