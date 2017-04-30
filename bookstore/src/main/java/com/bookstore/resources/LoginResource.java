1c34d42c050287e58821ab4146d848b3fc8b30cb
Faelivrin <jurasz.do@gmail.com>
2017-04-30 09:49:57 +0200

    Testing login

---
+++ bookstore/src/main/java/com/bookstore/resources/LoginResource.java
@@ -0,0 +1,37 @@
+package com.bookstore.resources;
+
+import com.bookstore.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * Created by Dominik on 29.04.2017.
+ */
+
+@RestController
+public class LoginResource {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/token")
+    public Map<String, String> token(HttpSession session, HttpServletRequest request){
+        System.out.println(request.getRemoteHost());
+
+        String remoteHost = request.getRemoteHost();
+        int portNumber = request.getRemotePort();
+
+        System.out.println(remoteHost + ":" + portNumber);
+        System.out.println(request.getRemoteAddr());
+
+
+        return Collections.singletonMap("token", session.getId());
+    }
+
+}
