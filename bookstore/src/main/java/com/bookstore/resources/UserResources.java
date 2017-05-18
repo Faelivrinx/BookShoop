4617cbe7c7a97c45be5498987d3d3f7469c5ab3f
Faelivrin <jurasz.do@gmail.com>
2017-05-18 07:41:27 +0200

    Added my profile

--- bookstore/src/main/java/com/bookstore/resources/UserResources.java
+++ bookstore/src/main/java/com/bookstore/resources/UserResources.java
@@ -1,5 +1,6 @@
 package com.bookstore.resources;
 
+import com.bookstore.config.SecurityConfig;
 import com.bookstore.config.SecurityUtility;
 import com.bookstore.domain.User;
 import com.bookstore.domain.security.Role;
@@ -11,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -43,11 +45,11 @@ public class UserResources {
         String username = mapper.get("username");
         String userEmail = mapper.get("email");
 
-        if(userService.findByUsername(username) != null){
+        if (userService.findByUsername(username) != null) {
             return new ResponseEntity("usernameExists", HttpStatus.BAD_REQUEST);
         }
 
-        if(userService.findByEmail(userEmail) != null){
+        if (userService.findByEmail(userEmail) != null) {
             return new ResponseEntity("emailExists", HttpStatus.BAD_REQUEST);
         }
 
@@ -78,7 +80,7 @@ public class UserResources {
 
         User user = userService.findByEmail(mapper.get("email"));
 
-        if(user == null){
+        if (user == null) {
             return new ResponseEntity("Email Not found", HttpStatus.BAD_REQUEST);
         }
 
@@ -93,5 +95,58 @@ public class UserResources {
         mailSender.send(newEmail);
 
         return new ResponseEntity("Email Sent!", HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/updateUserInfo", method = RequestMethod.POST)
+    public ResponseEntity profileInfo(@RequestBody HashMap<String, Object> mapper) throws Exception {
+
+        int id = (Integer) mapper.get("id");
+
+        String email = (String) mapper.get("email");
+        String username = (String) mapper.get("username");
+        String firstName = (String) mapper.get("firstName");
+        String lastName = (String) mapper.get("lastName");
+        String newPassword = (String) mapper.get("newPassword");
+        String currentPassword = (String) mapper.get("currentPassword");
+
+        User currentUser = userService.findById(Long.valueOf(id));
+
+        if(currentUser == null ) {
+            throw new Exception("User not found");
+        }
+
+        if(userService.findByEmail(email) != null){
+            if(userService.findByEmail(email).getId() != currentUser.getId()){
+                return new ResponseEntity("Email not found!", HttpStatus.BAD_REQUEST);
+            }
+        }
+
+        if(userService.findByUsername(username) != null){
+            if(userService.findByUsername(username).getId() != currentUser.getId()){
+                return new ResponseEntity("Username not found!", HttpStatus.BAD_REQUEST);
+            }
+        }
+
+        SecurityConfig securityConfig = new SecurityConfig();
+
+        if(newPassword != null && !newPassword.isEmpty() && !newPassword.equals("")){
+            BCryptPasswordEncoder passwordEncoder = SecurityUtility.passwordEncoder();
+            String dbPassword = currentUser.getPassword();
+            if(currentPassword.equals(dbPassword)){
+                currentUser.setPassword(passwordEncoder.encode(newPassword));
+            } else {
+                return new ResponseEntity("Incorrect current password!", HttpStatus.BAD_REQUEST);
+            }
+        }
+
+        currentUser.setFirestName(firstName);
+        currentUser.setLastName(lastName);
+        currentUser.setUsername(username);
+        currentUser.setEmail(email);
+
+        userService.save(currentUser);
+
+        return new ResponseEntity("Update Success", HttpStatus.OK);
+
     }
 }
--- store-front/src/app/components/my-profile/my-profile.component.ts
+++ store-front/src/app/components/my-profile/my-profile.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AppConst } from '../../constants/app-const';
+import { UserService } from '../../services/user.service';
+import { LoginService } from '../../services/login.service';
+import { User } from '../../models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-profile',
@@ -7,9 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyProfileComponent implements OnInit {
 
-  constructor() { }
+	private serverPath = AppConst.serverPath;
+	private fataFetched = false;
+	private loginError:boolean;
+	private loggedIn:boolean;
+	private credential = {'username': '', 'password':''};
+
+	private user: User = new User();
+	private updateSuccess:boolean;
+	private newPassword: string;
+	private incorrectPassword:boolean;
+
+  constructor(
+  	private loginService: LoginService, 
+  	private userService: UserService,
+  	private router: Router
+  	) { }
+
+  onUpdateUserInfo() {
+  	this.userService.updateUserInfo(this.user, this.newPassword).subscribe(
+  			res => {
+  				console.log(res.text());
+  				this.updateSuccess = true;
+  			}, error => {
+  				console.log(error.text());
+  				let errorMessage = error.text();
+  				if(errorMessage==="Incorrect current password!") this.incorrectPassword = true;
+  			}
+  		)
+  }
 
   ngOnInit() {
+  	this.loginService.checkSession().subscribe(
+  			res => {
+  				this.loggedIn = true;
+  			}, error => {
+  				this.loggedIn = false;
+  				console.log("inactive session");
+  				this.router.navigate(['/myAccount']);
+  			}
+  		);
   }
 
 }
